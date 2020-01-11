1b71361fe7eab229b5802a5ba0fe3b3c9a67b546
bzxg-space <58965227+bzxg-space@users.noreply.github.com>
2020-01-11 16:02:39 +0800

    Add files via upload

---
+++ js/copy.js
@@ -0,0 +1,10 @@
+function copy() {
+	var textArea = document.createElement("textarea");
+	textArea.innerHTML = word.innerHTML + '\n-- 来自一言';
+	document.body.appendChild(textArea);
+	textArea.select();
+	document.execCommand('copy');
+	toast('复制成功 : )', 0);
+	textArea.parentNode.removeChild(textArea);
+	//document.body.removeChild(textArea);
+}
